dfe34857f9adcb52d7586730031eeb8e9e0247f4
thomas <dev@homas-brunet.fr>
2016-04-06 10:40:57 +0200

    Update the sended request

--- inputs/src/main/java/senders/RequestSender.java
+++ inputs/src/main/java/senders/RequestSender.java
@@ -1,5 +1,7 @@
 package senders;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rabbitmq.client.Channel;
 import controller.Priority;
 import controller.Region;
 import controller.Request;
@@ -7,8 +9,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import resources.SummonerResource;
 import utils.Amqp;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.rabbitmq.client.Channel;
 
 /**
  * Created by Thomas on 20/11/2015.
@@ -20,11 +20,10 @@ public class RequestSender {
     public static void main(String[] args) throws Exception {
         //Connection to the amqp server
         Amqp amqp = Amqp.get();
-        Channel channel;
-        channel = Amqp.get().getChannel();
+        Channel channel = Amqp.get().getChannel();
         channel.queueDeclare(Amqp.QUEUE_TASK,false,false,false,null);
 
-        Request request = new Request(SummonerResource.getSummoners(new Integer[]{22253079}, Region.euw, Priority.LOW));
+        Request request = new Request(SummonerResource.getSummoners(Region.euw,new Integer[]{22253079,22169683}), Priority.LOW);
 
         //publish the json to the queue
         ObjectMapper mapper = new ObjectMapper();
